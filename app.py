dd2f47b8e5d21ea181f483594a7d52ce1b8bedf0
SATAN <34183912+SATANTESTBOT@users.noreply.github.com>
2018-09-02 12:29:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,15 @@ def handle_message(event):
     text = event.message.text #simplify for receove message
     sender = event.source.user_id #get user_id
     gid = event.source.sender_id #get group_id
-#=====[ LEAVE GROUP OR ROOM ]==========[ ARSYBAI ]======================
+    if text.startswith("/broadcast"):
+        separate = text.split(" ")
+        textt = text.replace(separate[0] + " ","")
+        if(event.source.user_id == "Udaa0a2f396dd41e4398b106d903d92fd"):
+            line_bot_api.reply_message(gid, TextSendMessage(text="ตั้งข้อความประกาศว่า " + textt))
+            groupcastt = textt
+            groupcast = {}
+        else:
+            pass
     try:
         if groupcastt != "":
             if groupcast[gid] == True:
@@ -100,7 +108,7 @@ def handle_message(event):
             else:
                 groupcast[gid] = True
     except Exception as Error:
-        line_bot_api.push_message(gid, TextSendMessage(text=Error))
+        line_bot_api.push_message(gid, TextSendMessage(text=Error + "\nกรุณาติดต่อแอดมิน"))
     if text.isdigit():
         b = int(text)
         reverse = 0
@@ -189,15 +197,6 @@ def handle_message(event):
                 except Exception as error:
                     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=error))
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=trs))
-    if text.startswith("/broadcast"):
-        separate = text.split(" ")
-        textt = text.replace(separate[0] + " ","")
-        if(event.source.user_id == "Udaa0a2f396dd41e4398b106d903d92fd"):
-            line_bot_api.reply_message(gid, TextSendMessage(text="ตั้งข้อความประกาศว่า " + textt))
-            groupcastt = textt
-            groupcast = {}
-        else:
-            pass
     if text == "/bye":
         if(event.source.user_id == "Udaa0a2f396dd41e4398b106d903d92fd"):
             confirm_template_message = TemplateSendMessage(
